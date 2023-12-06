da753603fe9950a2d68c7862879a547896fe9476
ChristoAW <99455915+ChristoAW@users.noreply.github.com>
2023-12-07 03:10:43 +0700

    add: template mail, pdf

--- server.js
+++ server.js
@@ -15,6 +15,57 @@ app.use(cors());
 app.get("/", (req, res) => {
   return res.send("Hello");
 });
+app.set("view engine", "ejs");
+app.get("/testmail", (req, res) => {
+  res.render("mail/ticket", {
+    event: {
+      title: "NAMA EVENT",
+      start: "16.00",
+      end: "18.00",
+      date: "24 November 2023",
+      loc: "TEMPAT EVENT",
+    },
+    nama: "test",
+    transaction_id: "TRANSC",
+    jumlah_ticket: 2,
+    wa: "0842148712",
+    email: "EMAIL",
+  });
+});
+app.get("/testpdf", (req, res) => {
+  res.render("pdf/ticket", {
+    item_id: "itemid",
+    header: "LINK IMAGE HEADER",
+    event_name: "EVENT NAME",
+    start: "16.00",
+    end: "18.00",
+    date: "32 DESEMBER 2023",
+    location: "LOCATION",
+    pages: [
+      {
+        ticket_number: 1,
+        ticket_token: "token1",
+        nama: "nama1",
+        email: "email1",
+        qr: "linkqr1",
+      },
+      {
+        ticket_number: 2,
+        ticket_token: "token2",
+        nama: "nama2",
+        email: "email2",
+        qr: "linkqr2",
+      },
+      {
+        ticket_number: 3,
+        ticket_token: "token3",
+        nama: "nama3",
+        email: "email3",
+        qr: "linkqr3",
+      },
+    ],
+  });
+});
 
 app.use("/user", require("./src/routes/user"));
 app.use("/events", require("./src/routes/event"));
