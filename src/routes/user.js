f2875d921c3ec9c687af525376d24f6b244b6e42
Koongh <yapkng@gmail.com>
2023-12-20 16:57:30 +0700

    Update user.js

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const router = express.Router();
 const userController = require("../controllers/userController");
-
+const historyController = require("../controllers/historyController");
+const scheduleController = require("../controllers/scheduleController");
 router.get("/check", userController.checkLogin);
 router.get("/google", userController.googleUser);
 router.get("/regis", userController.userRegis);
+router.get("/history", historyController.history);
+router.get("/schedule", scheduleController.schedule);
 
 module.exports = router;
