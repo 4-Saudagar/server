7aa9cf211a54f2e8b4f73f0d61e6d99b3ea9b1ec
Koongh <yapkng@gmail.com>
2023-12-20 14:38:35 +0700

    Update partner.js

--- src/routes/partner.js
+++ src/routes/partner.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const router = express.Router();
 const partnerController = require("../controllers/partnerController");
+const notifyController = require("../controllers/notifController");
 
 router.post("/add", partnerController.addPartners);
 router.get("/all", partnerController.getAllPartners);
 router.post("/allowPartner", partnerController.allowPartner);
+router.post("/notify", notifyController.notif);
 
 module.exports = router;
